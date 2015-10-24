d157f4cc240ebdcb82fab6b43c28e4296ce05ae9
Shiyang Ao <aosyang@hotmail.com>
2015-10-23 21:48:44 -0400

    Added basic intestine parts.

---
+++ HorrorGameJam/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConveyorBelt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        Rigidbody2D rb = coll.gameObject.GetComponent<Rigidbody2D>();
+
+        if (rb)
+        {
+            rb.velocity = new Vector2(1.0f, 0.0f);
+        }
+    }
+}
