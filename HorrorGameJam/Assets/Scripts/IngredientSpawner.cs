d6d603292acbf31d565aba05d19ce70220f756ea
Shiyang Ao <aosyang@hotmail.com>
2015-10-25 03:11:14 -0400

    Added spawn smoke. Added mouse dragging bottle.

--- HorrorGameJam/Assets/Scripts/IngredientSpawner.cs
+++ HorrorGameJam/Assets/Scripts/IngredientSpawner.cs
@@ -6,6 +6,7 @@ public class IngredientSpawner : MonoBehaviour
     public GameObject[] ingredients;
     public GameObject smokeEffect;
     float spawnTime;
+    public GameObject spawnEffect;
 
     // Use this for initialization
     void Start()
@@ -20,11 +21,21 @@ public class IngredientSpawner : MonoBehaviour
         {
             if (ingredients.GetLength(0) > 0)
             {
-                int index = Random.Range(0, ingredients.GetLength(0));
-                GameObject go = Instantiate(ingredients[index], transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f))) as GameObject;
+                Invoke("SpawnObject", 0.5f);
+
+                if (spawnEffect)
+                {
+                    Instantiate(spawnEffect, transform.position, Quaternion.identity);
+                }
             }
 
             spawnTime = Time.time + 2.0f;
         }
+    }
+
+    void SpawnObject()
+    {
+        int index = Random.Range(0, ingredients.GetLength(0));
+        GameObject go = Instantiate(ingredients[index], transform.position, Quaternion.Euler(0.0f, 0.0f, Random.Range(0.0f, 360.0f))) as GameObject;
     }
 }
--- HorrorGameJam/Assets/Scripts/JuiceBottleScript.cs
+++ HorrorGameJam/Assets/Scripts/JuiceBottleScript.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class JuiceBottleScript : MonoBehaviour {
+public class JuiceBottleScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnMouseDrag()
+    {
+        HingeJoint2D hinge = GetComponent<HingeJoint2D>();
+
+        if (!hinge)
+        {
+            hinge = gameObject.AddComponent<HingeJoint2D>();
+        }
+
+        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        hinge.connectedAnchor = cursorPosition;
+    }
+
+    void OnMouseUp()
+    {
+        HingeJoint2D hinge = GetComponent<HingeJoint2D>();
+
+        if (hinge)
+        {
+            Destroy(hinge);
+        }
+    }
 }
